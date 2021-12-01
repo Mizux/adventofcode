60ce8324a455430c7b9c83db30eca02ea20bf86d
Mizux Seiha <mizux.dev@gmail.com>
2021-12-01 15:21:16 +0100

    cleanup day_1/second.py

--- day_1/second.py
+++ day_1/second.py
@@ -1,26 +1,18 @@
 #!/usr/bin/env python3
 
-count = 0
+SLIDE_WINDOWS = 3
 
 f = open("input.txt", "r")
-a_one = int(f.readline())
-a_two = int(f.readline())
-a_three = int(f.readline())
+window = []
+for i in range(SLIDE_WINDOWS):
+    window.append(int(f.readline()))
 
-b_one = a_two
-b_two = a_three
-b_three = 0
-for x in f:
-    print(f'current: {x}')
-    b_three = int(x)
-
-    if sum([b_one, b_two, b_three]) > sum([a_one, a_two, a_three]):
+count = 0
+for cur in f:
+    #print(f'current: {cur}')
+    window.append(int(cur))
+    if sum(window[1:]) > sum(window[0:-1]):
         count +=1
-    a_one = a_two
-    a_two = a_three
-    a_three = b_three
-    b_one = b_two
-    b_two = b_three
-    b_three = 0
+    window.pop(0)
 
 print(f'increase: {count}')
