e9e643ec0dd8e30f7dd3cd8340f8ae4a896050c4
Mizux Seiha <mizux.dev@gmail.com>
2021-12-05 09:09:25 +0100

    Add day_5

---
+++ day_5/first.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import re
+from collections import defaultdict
+
+FILE='test.txt' # sol: 5
+FILE='input.txt' # sol: 3990
+
+pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
+
+def parse_input(file):
+    with open(file, 'r') as f:
+        values = []
+        for line in f:
+            match = pattern.search(line)
+            values.append([int(i) for i in match.groups()])
+    return values
+
+
+values = parse_input(FILE)
+#print(values)
+wind = defaultdict(int)
+for value in values:
+    [x1, y1, x2, y2] = value
+    #print(f'{x1},{y1} -> {x2},{y2}')
+
+    if x1 == x2:
+        #print('vertical')
+        for y in range(min(y1, y2), max(y1,y2)+1):
+            wind[(x1, y)] += 1
+            #print(f'wind[({x1}, {y})] += 1')
+    elif y1 == y2:
+        #print('horizontal')
+        for x in range(min(x1, x2), max(x1, x2)+1):
+            wind[(x, y1)] += 1
+            #print(f'wind[({x}, {y1})] += 1')
+    else:
+        #print('can\'t deal with it')
+        continue
+
+#print(wind)
+count = 0
+for key, value in wind.items():
+    #print(key, value)
+    if value > 1:
+        count += 1
+print(f'result: {count}')
---
+++ day_5/second.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import re
+from collections import defaultdict
+
+FILE='test.txt' # sol: 12
+FILE='input.txt' # sol: 21305
+
+pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
+
+def parse_input(file):
+    with open(file, 'r') as f:
+        values = []
+        for line in f:
+            match = pattern.search(line)
+            values.append([int(i) for i in match.groups()])
+    return values
+
+
+def is_diag(x1, y1, x2, y2):
+    if abs(x2-x1) == abs(y2-y1):
+        return True
+    return False
+
+
+values = parse_input(FILE)
+#print(values)
+wind = defaultdict(int)
+for value in values:
+    [x1, y1, x2, y2] = value
+    #print(f'{x1},{y1} -> {x2},{y2}')
+
+    if x1 == x2:
+        #print('vertical')
+        for y in range(min(y1, y2), max(y1,y2)+1):
+            wind[(x1, y)] += 1
+            #print(f'wind[({x1}, {y})] += 1')
+    elif y1 == y2:
+        #print('horizontal')
+        for x in range(min(x1, x2), max(x1, x2)+1):
+            wind[(x, y1)] += 1
+            #print(f'wind[({x}, {y1})] += 1')
+    elif is_diag(x1, y1, x2, y2):
+        #print('diagonal')
+        x_dir = 1 if x1 < x2 else -1
+        y_dir = 1 if y1 < y2 else -1
+        delta = abs(x2-x1)+1
+        for i in range(delta):
+            x = x1 + i * x_dir
+            y = y1 + i * y_dir
+            wind[(x, y)] += 1
+            #print(f'wind[({x}, {y})] += 1')
+    else:
+        #print('can\'t deal with it')
+        continue
+
+#print(wind)
+count = 0
+for key, value in wind.items():
+    #print(key, value)
+    if value > 1:
+        count += 1
+print(f'result: {count}')
