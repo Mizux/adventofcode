9256061902c7f8f3fad3a4a57d19193d91532041
Mizux Seiha <mizux.dev@gmail.com>
2021-12-08 08:16:18 +0100

    cleanup

--- day_8/first.py
+++ day_8/first.py
@@ -3,12 +3,11 @@
 FILE='test.txt' # sol: 26
 FILE='input.txt' # sol: 504
 
-# split / rstrip()
 def parse_input(file):
     out = [];
     with open(file, 'r') as f:
         for line in f:
-            digits = line.rstrip().split(" | ")[1].strip().split(' ')
+            digits = line.rstrip().split(" | ")[1].split(' ')
             #print(f'line digits: {digits}')
             out.extend(digits)
     #print(f'all digits: {out}')
@@ -16,5 +15,5 @@ def parse_input(file):
 
 
 entry = parse_input(FILE)
-count = len([digit for digit in entry if len(digit) in [2, 4, 3, 7]])
+count = len([digit for digit in entry if len(digit) in [2, 4, 3, 7]]) # 1 4 7 8
 print(f'count {count}')
